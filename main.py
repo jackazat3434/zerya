d810b7d3f7a8bf3f238f73d38f03e7a7e7caad39
gktg0011 <143345012+gktg0011@users.noreply.github.com>
2023-10-03 15:20:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -357,8 +357,8 @@ async def tokatla(event):
   yiyen_mesaj = await mesaj.get_reply_message()
   yiyen = yiyen_mesaj.sender
 
-  vuran = f"{atan.id==1550788256}"
-  atilan = f"{yiyen.id==5375589992}"
+  vuran = f"{atan.{}}"
+  atilan = f"{yiyen.{}}"
 
   memom = random.choice(tokat_mesaj)
   await event.reply(memom.format(vuran, atilan))

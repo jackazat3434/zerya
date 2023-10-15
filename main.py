56659204c6ee2f61080e2e69d58edb82e758696d
ShinkaiSenseii <143345012+UlgazSensei@users.noreply.github.com>
2023-10-15 21:23:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1042,7 +1042,7 @@ async def tokatla(event):
 async def start(event):
   if event.is_private:
     async for usr in client.iter_participants(event.chat_id):
-     ad = f"**👋🏻 Merhaba**"
+     ad = f"{first_name}"
      await event.reply(f"{ad} {startmesaj}", buttons=(
                       [
                        Button.inline("💫 ᴋᴏᴍᴜᴛʟᴀʀ", data="help")
